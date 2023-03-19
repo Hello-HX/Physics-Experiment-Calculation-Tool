005c8c0cb221f6aed516865a86c7a62a734f4007
GUN_HX <111850224+Hello-HX@users.noreply.github.com>
2023-03-19 11:58:04 +0800

    Update tool.c

--- tool.c
+++ tool.c
@@ -15,66 +15,94 @@ int main()
     double derta, B;                          // derta为实验仪器允许的最大误差，B存放B类不确定度的计算结果
     double U, R;                              // U存放合成不确定度，R存放相对不确定度
 
-    puts("欢迎来到我的大学物理实验小工具！\n");
-    
     while (1)
-    {
-        printf("请输入数据个数:"); // 输入数据个数提示
-        scanf("%d", &n);
+    {   
         sum = 0;
         sum1 = 0;
+        printf("\033[31m");
+        printf("<><><><><><><><><><><><><><><><><><><><><><>\n");
+        puts("@CHX->欢迎来到我的大学物理实验小工具！");
+        printf("\033[m");
+        
+        printf("|step1--\033[33m请输入数据个数:\033[m"); // 输入数据个数提示
+        scanf("%d", &n);
+        
         // 求原始数据的算术平均值
-        printf("请输入原始数据:"); // 输入提示
+        printf("|step2--\033[33m请输入原始数据:\033[m"); // 输入提示
         for (i = 1; i <= n; i++)
         {
             scanf("%lf", &data[i]);
             sum += data[i];
         }
-        printf("\n");
-        average = sum / n;
-        printf("所求数据平均值为:%f\n\n", average);
 
-        printf("\n");
+        printf("|step3--\033[33m请输入零点读数：\033[m");
+        double d = 0;
+        scanf("%lf",&d);
+        average = sum / n - d;
+        printf("\033[31m");
+        printf("  <>@CHX>>>>所求数据平均值为:%f\n", average);
+
         // 求A类不确定度
         for (i = 1; i <= n; i++)
         {
             sum1 += pow(data[i] - average, 2);
         }
         A = sqrt(sum1 / ((double)n * ((double)n - 1)));
-        printf("A类不确定度为:%lf\n\n", A);
+        printf("  <>@CHX>>>>A类不确定度为:%lf\n", A);
+        printf("\033[m");
 
-        printf("\n");
         // 求B类仪器误差不确定度
-        printf("请输入实验仪器误差:\n");
-        printf("\t直尺0.2\n");
-        printf("\t游标卡尺: 50=0.02mm 20=0.05mm 10=0.01mm\n");
-        printf("\t螺旋：0-100=0.004mm 100-150=0.005mm\n");
-        printf("\t天平：最小分度一半0.1\n");
-        printf("\t显微镜：0.005mm\n");
-        printf("\t数字类：仪器最小读数\n");
-        printf("\t计时类：0.3s\n");
-        printf("\t分光计：最小分度\n");
-        printf("\t温度计：0.1g\n");
-        printf("\t万用表不用\n");
+        printf("|step4--\033[33m请输入实验仪器误差:\033[m\n");
+        printf("  |直尺0.5\n");
+        printf("  |游标卡尺: 50=0.02mm 20=0.05mm 10=0.01mm\n");
+        printf("  |螺旋：0-100=0.004mm 100-150=0.005mm\n");
+        printf("  |天平：最小分度一半0.1\n");
+        printf("  |显微镜：0.005mm\n");
+        printf("  |数字类：仪器最小读数\n");
+        printf("  |计时类：0.3s\n");
+        printf("  |分光计：最小分度\n");
+        printf("  |温度计：0.1g\n");
+        printf("  |万用表不用\n");
+        printf("  |拉力计：0.005kg\n");
+        printf("  |电阻箱子：读数×等级3+最小刻度×3\n");
+        printf("  \033[33m||请输入->\033[m");
         scanf("%lf", &derta);
         B = derta / sqrt(3);
-        printf("B类仪器误差不确定度为:%f\n\n", B);
+        printf("\033[31m");
+        printf("  @CHX>>>>B类仪器误差不确定度为:%f\n", B);
+         printf("\033[m");
 
-        printf("\n");
         // 求合成不确定度
-        puts("保留位数，分度值下一位，不估读外\n");
-        puts("四舍六入五左右\n");
-        puts("五右边非0，就进\n");
-        puts("否则看左，奇入偶舍\n");
-        printf("请分别输入A类和B类不确定度:");
+        printf("|step5--\033[33m请分别输入A类和B类不确定度:\033[m\n");
+        puts("  |保留位数，分度值下一位，不估读外");
+        puts("  |四舍六入五左右");
+        puts("  |五右边非0，就进");
+        puts("  |否则看左，奇入偶舍");
+        printf("  |看有效位\n");
+        printf("  \033[33m||请输入->\033[m");
         scanf("%lf%lf", &A, &B);
         U = sqrt(pow(A, 2) + pow(B, 2));
-        printf("合成不确定度为:%lf\n\n", U);
+        printf("\033[31m");
+        printf("  <>@CHX>>>>合成不确定度为:%lf\n", U);
+         printf("\033[m");
 
 
         // 求相对不确定度
         R = U / average;
-        printf("相对不确定度为:%f\n\n", R);
+        printf("\033[31m");
+        printf("  <>@CHX>>>>相对不确定度为:%f\n", R);
+         printf("\033[m");
+        
+
+        //数据整合
+        printf("|step6--\033[33m数据整合情况如下：\033[m\n");
+        printf("\033[31m");
+        printf("  <>@CHX>>>>所求数据平均值为:%f\n", average);
+        printf("  <>@CHX>>>>A类不确定度为:%lf\n", A);
+        printf("  <>@CHX>>>>B类仪器误差不确定度为:%f\n", B);
+        printf("  <>@CHX>>>>合成不确定度为:%lf\n", U);
+        printf("  <>@CHX>>>>相对不确定度为:%f\n", R);
+         printf("\033[m");
 
         printf("\n\n");
     }
